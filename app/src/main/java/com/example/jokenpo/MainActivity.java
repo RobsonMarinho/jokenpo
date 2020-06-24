df1e34860f86c9790bd068d5a277fe0281345c40
Robson Marinho <robson_luz15@hotmail.com>
2020-06-24 15:15:21 -0300

    Escolhendo a opção [Pedra,Papel,Tesoura] e setando os métodos

--- app/src/main/java/com/example/jokenpo/MainActivity.java
+++ app/src/main/java/com/example/jokenpo/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.jokenpo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view) {
+        this.opcaoSelecionada("Pedra");
+    }
+    public void selecionadoPapel(View view) {
+        this.opcaoSelecionada("Papel");
+    }
+    public void selecionadoTesoura(View view) {
+        this.opcaoSelecionada("Tesoura");
+    }
+    public void opcaoSelecionada(String opcaoSelecionada){
+        System.out.println("Item clicado: " + opcaoSelecionada);
+
     }
 }
